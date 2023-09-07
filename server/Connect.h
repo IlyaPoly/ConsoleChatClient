77c980bf2fa4fd326750da46b93ed2244f3d8b78
IlyaPoly <132166094+IlyaPoly@users.noreply.github.com>
2023-09-07 13:37:06 +0300

    Create Connect.h

---
+++ server/Connect.h
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<unistd.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+
+#define MESSAGE_LENGTH 1024 // Максимальный размер буфера для данных
+#define PORT 700 // Будем использовать этот номер порта
+#define PORT2 30000
+
+ class ConnectTCP
+ {
+    struct sockaddr_in serveraddress, client;
+    socklen_t length;
+    int sockert_file_descriptor, connection, bind_status, connection_status;
+public:
+    ConnectTCP()
+    {
+        sockert_file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+        serveraddress.sin_addr.s_addr = htonl(INADDR_ANY);
+        serveraddress.sin_port = htons(PORT);
+        serveraddress.sin_family = AF_INET;
+    
+    }
+    int connect()
+    {
+        bind_status = bind(sockert_file_descriptor, (struct sockaddr*)&serveraddress, sizeof(serveraddress));
+        if(bind_status == -1)
+        {
+            std::cout << "Socket binding failed.!" << std::endl;
+            exit(1);
+        }
+    return sockert_file_descriptor;
+    }
+ };
+ void work(int& connection, Chat& chat)
+ {
+     bool on = false;
+     do {
+         if (chat.getSelectedUser())
+         	chat.userMenu(connection);
+         else 
+         	chat.mainMenu(connection, on);
+     } while (!on);
+ }
